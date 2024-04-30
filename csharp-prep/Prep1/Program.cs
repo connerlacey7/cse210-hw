426470d79f74eaddf0cf8aa01451c3471e09b346
connerlacey7 <lac17001@byui.edu>
2024-04-29 19:00:42 -0600

    Name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name?");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your Last Name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstName} {lastname}");
     }
 }
