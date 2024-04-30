d0359e5e55a37b4f5a5910938639d1bf32fb1673
connerlacey7 <lac17001@byui.edu>
2024-04-29 19:17:48 -0600

    Grade system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade Percentage?");
+        float gradePercentage = float.Parse(Console.ReadLine());
+
+        if (gradePercentage > 90)
+        {
+            Console.WriteLine("You got an A. Good Job.");
+        }
+        else if (gradePercentage >= 80 && gradePercentage <= 89)
+        {
+            Console.WriteLine("Your grade is a B");
+        }
+        else if (gradePercentage >= 70 && gradePercentage <= 79)
+        {
+            Console.WriteLine("Your grade is a C");
+        } 
+        else if (gradePercentage >= 60 && gradePercentage <= 69)
+        {
+            Console.WriteLine("Your grade is a D");
+        }  
+        else if(gradePercentage <= 59)
+        {
+            Console.WriteLine("Your grade is a F");
+        } 
+
+        if (gradePercentage > 70)
+        {
+            Console.WriteLine("You passed the class! Good Job");
+        }                      
+        else
+        {
+            Console.WriteLine("You didn't pass this time. Bettre luck next time.");
+        }
     }
 }
